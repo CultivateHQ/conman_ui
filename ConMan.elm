dfeb9b79b789555b547e52908b8e17ec2ded5827
Alan Gardner <urfolomeus@gmail.com>
2015-08-31 14:16:14 +0100

    Extracting the Contact

--- ConMan.elm
+++ ConMan.elm
@@ -1,33 +1,7 @@
 module ConMan where
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Contact exposing (view, Model)
 
 
 main =
   view (Model "Bobby Tables" "bobby@example.com" "01 234 5678")
-
-
--- MODEL
-
-type alias Model =
-  { name: String
-  , email: String
-  , phone: String
-  }
-
-
--- VIEW
-
-view contact =
-  div [ class "contact" ]
-  [ h2 [ class "contact__name" ] [ text contact.name ]
-  , div [ class "contact__email" ]
-    [ span [ ] [ text "Email:" ]
-    , a [ href ("mailto:" ++ contact.email) ] [ text contact.email ]
-    ]
-  , div [ class "contact__phone" ]
-    [ span [ ] [ text "Phone:" ]
-    , a [ href ("tel:" ++ contact.phone) ] [ text contact.phone ]
-    ]
-  ]
---
+++ Contact.elm
@@ -0,0 +1,29 @@
+module Contact where
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+-- MODEL
+
+type alias Model =
+  { name: String
+  , email: String
+  , phone: String
+  }
+
+
+-- VIEW
+
+view contact =
+  div [ class "contact" ]
+  [ h2 [ class "contact__name" ] [ text contact.name ]
+  , div [ class "contact__email" ]
+    [ span [ ] [ text "Email:" ]
+    , a [ href ("mailto:" ++ contact.email) ] [ text contact.email ]
+    ]
+  , div [ class "contact__phone" ]
+    [ span [ ] [ text "Phone:" ]
+    , a [ href ("tel:" ++ contact.phone) ] [ text contact.phone ]
+    ]
+  ]
