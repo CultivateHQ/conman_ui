4658a77971b55276743bfb3a43276bbc3aa4573a
Alan Gardner <urfolomeus@gmail.com>
2015-08-31 14:08:08 +0100

    Introducing a Model

--- ConMan.elm
+++ ConMan.elm
@@ -5,20 +5,29 @@ import Html.Attributes exposing (..)
 
 
 main =
-  view
+  view (Model "Bobby Tables" "bobby@example.com" "01 234 5678")
+
+
+-- MODEL
+
+type alias Model =
+  { name: String
+  , email: String
+  , phone: String
+  }
 
 
 -- VIEW
 
-view =
+view contact =
   div [ class "contact" ]
-  [ h2 [ class "contact__name" ] [ text "Bobby Tables" ]
+  [ h2 [ class "contact__name" ] [ text contact.name ]
   , div [ class "contact__email" ]
     [ span [ ] [ text "Email:" ]
-    , a [ href ("mailto:bobby@example.com") ] [ text "bobby@example.com" ]
+    , a [ href ("mailto:" ++ contact.email) ] [ text contact.email ]
     ]
   , div [ class "contact__phone" ]
     [ span [ ] [ text "Phone:" ]
-    , a [ href ("tel:01 234 5678") ] [ text "01 234 5678" ]
+    , a [ href ("tel:" ++ contact.phone) ] [ text contact.phone ]
     ]
   ]
