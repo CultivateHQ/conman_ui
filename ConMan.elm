8eb595ce0382b71f37232733a990a7d5e8e9ad91
Alan Gardner <urfolomeus@gmail.com>
2015-08-31 13:04:53 +0100

    Building a Contact View

--- ConMan.elm
+++ ConMan.elm
@@ -11,4 +11,14 @@ main =
 -- VIEW
 
 view =
-  text "ConMan is in a View!"
+  div [ class "contact" ]
+  [ h2 [ class "contact__name" ] [ text "Bobby Tables" ]
+  , div [ class "contact__email" ]
+    [ span [ ] [ text "Email:" ]
+    , a [ href ("mailto:bobby@example.com") ] [ text "bobby@example.com" ]
+    ]
+  , div [ class "contact__phone" ]
+    [ span [ ] [ text "Phone:" ]
+    , a [ href ("tel:01 234 5678") ] [ text "01 234 5678" ]
+    ]
+  ]
